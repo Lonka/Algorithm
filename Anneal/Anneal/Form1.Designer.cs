7009215e6d54067c7961c1fb522ee4c94ca6b31c
Lonka <lonka.liu@gmail.com>
2015-05-19 09:12:45 +0800

    練習退火演算法

---
+++ Anneal/Anneal/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Anneal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lsb_Data = new System.Windows.Forms.ListBox();
+            this.btn_Execute = new System.Windows.Forms.Button();
+            this.lsb_Ans = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lsb_Data
+            // 
+            this.lsb_Data.FormattingEnabled = true;
+            this.lsb_Data.ItemHeight = 12;
+            this.lsb_Data.Location = new System.Drawing.Point(12, 23);
+            this.lsb_Data.Name = "lsb_Data";
+            this.lsb_Data.Size = new System.Drawing.Size(371, 412);
+            this.lsb_Data.TabIndex = 0;
+            // 
+            // btn_Execute
+            // 
+            this.btn_Execute.Location = new System.Drawing.Point(389, 23);
+            this.btn_Execute.Name = "btn_Execute";
+            this.btn_Execute.Size = new System.Drawing.Size(75, 23);
+            this.btn_Execute.TabIndex = 1;
+            this.btn_Execute.Text = "執行";
+            this.btn_Execute.UseVisualStyleBackColor = true;
+            this.btn_Execute.Click += new System.EventHandler(this.btn_Execute_Click);
+            // 
+            // lsb_Ans
+            // 
+            this.lsb_Ans.FormattingEnabled = true;
+            this.lsb_Ans.ItemHeight = 12;
+            this.lsb_Ans.Location = new System.Drawing.Point(389, 47);
+            this.lsb_Ans.Name = "lsb_Ans";
+            this.lsb_Ans.Size = new System.Drawing.Size(283, 388);
+            this.lsb_Ans.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 449);
+            this.Controls.Add(this.lsb_Ans);
+            this.Controls.Add(this.btn_Execute);
+            this.Controls.Add(this.lsb_Data);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lsb_Data;
+        private System.Windows.Forms.Button btn_Execute;
+        private System.Windows.Forms.ListBox lsb_Ans;
+    }
+}
+
---
+++ Anneal/Anneal/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Anneal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Dictionary<string, int> disData;
+        private List<string> worstPath = new List<string>();
+        private void btn_Execute_Click(object sender, EventArgs e)
+        {
+            lsb_Data.Items.Clear();
+            Dictionary<string, string> disList = new Dictionary<string, string>();
+            disList["A"] = "0,25,15,33,18";
+            disList["B"] = "25,0,9,16,22";
+            disList["C"] = "15,9,0,30,25";
+            disList["D"] = "33,16,30,0,19";
+            disList["E"] = "18,22,25,19,0";
+            
+
+            int exeCount = 0;
+            int exeMax = 100;
+            while (exeCount < exeMax)
+            {
+                List<string> path = new List<string>();
+                worstPath = new List<string>();
+                path.Add("A");
+                path.Add("B");
+                path.Add("C");
+                path.Add("D");
+                path.Add("E");
+
+                int temp = 2000;
+                int deltaTemp = 100;
+                double fitnessProbability = 0.9;
+                double deltaFitnessProbability = 0.9;
+                int errorCount = 0;
+                int fitnessCount = 0;
+
+                disData = InitialDistance(disList); ;
+                int bastDistance = CalculateDistance(path);
+
+                while (temp > 0)
+                {
+                    lsb_Data.Items.Add("temp:" + temp);
+                    while (errorCount < 3 && fitnessCount < 100)
+                    {
+                        fitnessCount++;
+                        lsb_Data.Items.Add("errorCount:" + errorCount);
+                        List<string> nextPath = randomChange(path);
+                        int nextDistance = CalculateDistance(nextPath);
+                        if (nextDistance > bastDistance)
+                        {
+                            double randomFitnessProbability = new Random().Next(0, 999) * 0.001;
+                            if (randomFitnessProbability > fitnessProbability)
+                            {
+                                errorCount++;
+                                worstPath.Add(string.Join("-", nextPath.ToArray()));
+                                continue;
+                            }
+                        }
+                        path = nextPath;
+                        bastDistance = nextDistance;
+                        lsb_Data.Items.Add(string.Join("-", path.ToArray()) + "," + bastDistance);
+                        Application.DoEvents();
+                    }
+                    temp -= deltaTemp;
+                    fitnessProbability *= deltaFitnessProbability;
+                    errorCount = 0;
+                    fitnessCount = 0;
+                    Application.DoEvents();
+                }
+                if (!lsb_Ans.Items.Contains(string.Join("-", path.ToArray()) + "," + bastDistance))
+                    lsb_Ans.Items.Add(string.Join("-", path.ToArray()) + "," + bastDistance);
+                exeCount++;
+            }
+        }
+
+        private Dictionary<string, int> InitialDistance(Dictionary<string, string> disList)
+        {
+            Dictionary<string, int> result = new Dictionary<string, int>();
+            foreach (KeyValuePair<string, string> item in disList)
+            {
+                string[] values = item.Value.Split(',');
+                for (int i = 0; i < values.Length; i++)
+                {
+                    if (!values[i].Trim().Equals("0"))
+                    {
+                        int disPoint = -1;
+                        int.TryParse(values[i], out disPoint);
+                        result.Add(item.Key + "-" + Convert.ToChar(65 + i), disPoint);
+                    }
+                }
+            }
+            return result;
+        }
+
+        private int CalculateDistance(List<string> path)
+        {
+            int disCount = 0;
+            for (int i = 0; i < path.Count; i++)
+            {
+                int nextPath = i + 1;
+                disCount += disData[path[i] + "-" + path[(nextPath > path.Count - 1 ? 0 : nextPath)]];
+            }
+            return disCount;
+        }
+
+        private List<string> randomChange(List<string> path)
+        {
+            List<string> result = new List<string>();
+            do
+            {
+                result = new List<string>(path);
+                int pointA = new Random(Guid.NewGuid().GetHashCode()).Next(0, 4);
+                int pointB = -1;
+                do
+                {
+                    pointB = new Random(Guid.NewGuid().GetHashCode()).Next(0, 4);
+                } while (pointB == -1 && pointB == pointA);
+                string tempPoint = result[pointA];
+                result[pointA] = result[pointB];
+                result[pointB] = tempPoint;
+            } while (worstPath.Contains(string.Join("-", result.ToArray())));
+            return result;
+        }
+    }
+}
